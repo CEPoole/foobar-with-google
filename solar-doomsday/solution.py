a4d7dfe1acb6be48845aefcdd9617f72a95276da
Chris Poole <CEPoole@users.noreply.github.com>
2020-12-11 13:24:22 +0000

    Solution 1: Solar Doomsday

--- solar-doomsday/solution.py
+++ solar-doomsday/solution.py
@@ -1,3 +1,19 @@
+import math
 
-def​ ​solution(area):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+
+def solution(area):
+    panels = []
+
+    def loop(remaining):
+        current = math.floor(math.sqrt(remaining))
+
+        panel = int(current * current)
+        panels.append(panel)
+
+        new = remaining - panel
+
+        if new != 0:
+            loop(new)
+
+    loop(area)
+    return panels
