ab657bb0019f5f8af6cb039eddb9029c95affbb9
Chris Poole <CEPoole@users.noreply.github.com>
2020-12-11 15:36:01 +0000

    Solution 2: Numbers Station Coded Messages

--- numbers-station-coded-messages/solution.py
+++ numbers-station-coded-messages/solution.py
@@ -1,2 +1,7 @@
-def​ ​solution(l,​ ​t):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+
+def solution(l, t):
+    for i in range(len(l) + 1):
+        for j in range(i, len(l) + 1):
+            if sum(l[i:j]) == t:
+                return [i, j - 1]
+    return [-1, -1]
